288237656f8edc3f7816d468aa6a565de2227992
Tony Morris <Tony.Morris@hyland.com>
2020-12-06 00:39:41 -0500

    feat: Initial add of Day 3

---
+++ day03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code: Day 3");
+
+            string[] input = File.ReadAllLines("input.txt");
+        }
+    }
+}
