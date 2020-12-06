2ef3fd62efac66576d1342ef51c32d8a2d76996b
Tony Morris <Tony.Morris@hyland.com>
2020-12-06 00:49:30 -0500

    feat: Day 3 Part 1

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace day03
 {
@@ -10,6 +11,36 @@ namespace day03
             Console.WriteLine("Advent of Code: Day 3");
 
             string[] input = File.ReadAllLines("input.txt");
+
+            int totalHeight = input.Length;
+            int initialWidth = input[0].Length;
+            int minWidth = totalHeight * 3;
+            int multiplier = (minWidth / initialWidth) + 1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = string.Concat(Enumerable.Repeat(input[i], multiplier));
+            }
+
+            int xOffset = 0;
+            int noTree = 0;
+            int tree = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char spot = input[i][xOffset];
+                if (spot == '.')
+                {
+                    noTree++;
+                }
+                else if (spot == '#')
+                {
+                    tree++;
+                }
+
+                xOffset += 3;
+            }
+
+            Console.WriteLine(tree);
         }
     }
 }
